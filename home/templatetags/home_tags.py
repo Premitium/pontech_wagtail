8b444cc760d2d3c5e7e1925ef923561da2ee15c6
Premitium <simeonparvanov@ymail.com>
2017-09-04 15:32:11 +0300

    Added breadcrumbs

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -133,12 +133,12 @@ def adverts(context):
 @register.inclusion_tag('home/tags/breadcrumbs.html', takes_context=True)
 def breadcrumbs(context):
     self = context.get('self')
-    if self is None or self.depth <= 2:
+    if self is None or self.depth <= 3:
         # When on the home page, displaying breadcrumbs is irrelevant.
         ancestors = ()
     else:
         ancestors = Page.objects.ancestor_of(
-            self, inclusive=True).filter(depth__gt=2)
+            self, inclusive=True).filter(depth__gt=3)
     return {
         'ancestors': ancestors,
         'request': context['request'],
