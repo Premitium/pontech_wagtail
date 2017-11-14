8240caaa10a6a6e8f80bfc82c50a86418e1408f6
Premitium <simeonparvanov@ymail.com>
2017-11-14 11:34:18 +0200

    When DEBUG = False Django doesn't serve static files but NGINX

--- pontechwagtailtrans/settings/base.py
+++ pontechwagtailtrans/settings/base.py
@@ -17,7 +17,7 @@ import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Quick-start development settings - unsuitable for production
